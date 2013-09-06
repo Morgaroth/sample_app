017fb0ea49596f95172aa61cc910f6826b9c042a
Morgarooth <mateuszjaje@gmail.com>
2013-09-06 16:19:46 +0200

    Excercises from chapter 3.3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) { "RoR Tutorial Sample App" }
 
   describe "Home page" do
 
@@ -9,9 +11,9 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title content 'RoR Tutorial Sample App | Home'" do
+    it "should have the title content 'Home" do
       visit '/static_pages/home'
-      expect(page).to have_title("RoR Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -22,9 +24,9 @@ describe "StaticPages" do
       expect(page).to have_content('Help')
     end
 
-    it "should have the title content 'RoR Tutorial Sample App | Help'" do
+    it "should have the title content 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("RoR Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -35,9 +37,22 @@ describe "StaticPages" do
       expect(page).to have_content('About Us')
     end
 
-    it "should have the title content 'RoR Tutorial Sample App | About Us'" do
+    it "should have the title content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("RoR Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'email'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('email')
+    end
+
+    it "should have the title content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
